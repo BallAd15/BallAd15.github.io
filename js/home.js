b9a3e7471cb79344477eb127106834887c46654e
Aditya Baliga <61866962+BallAd15@users.noreply.github.com>
2023-11-13 17:27:01 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,62 @@
+function openOverlay() {
+    document.getElementsByClassName("overlay")[0].style.height = "100vh";
+  }
+  
+function closeOverlay() {
+    document.getElementsByClassName("overlay")[0].style.height = "12%";
+    document.getElementsByClassName("overlay-about")[0].style.display = "none";
+    document.getElementsByClassName("overlay-arrow")[0].style.display = "none";
+    document.getElementsByClassName("navbar")[0].style.display = "flex";
+}
+
+function menubarShow(){
+    const hiddenDiv = document.getElementById("nvbm");
+    const menuIcon = document.getElementsByClassName("menubar-icon")[0];
+    if ( hiddenDiv.classList.contains('navbar-menu')) {
+        hiddenDiv.classList.remove('navbar-menu');
+        hiddenDiv.classList.add('visible');
+        document.getElementsByClassName("overlay")[0].style.height = "315px";
+        document.getElementsByClassName("site-title")[0].style.borderBottom = "1px solid rgb(99, 95, 95)";
+        menuIcon.textContent="expand_less"
+    } else {
+        hiddenDiv.classList.remove('visible');
+        hiddenDiv.classList.add('navbar-menu');
+        document.getElementsByClassName("overlay")[0].style.height = "12%";
+        document.getElementsByClassName("site-title")[0].style.borderBottom = "none";
+        menuIcon.textContent="menu"
+    }
+}
+
+//Smooth scrolling when redirecting
+document.addEventListener('click', function (e) {
+    if (e.target.tagName === 'A') {
+        e.preventDefault();
+        var targetId = e.target.getAttribute('href').substring(1); 
+        var targetSection = document.getElementById(targetId); 
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth'}); // 4000ms -> 2s
+        }
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const formGroups = document.querySelectorAll(".form-group");
+  
+    formGroups.forEach((formGroup) => {
+      const input = formGroup.querySelector(".form-control");
+  
+      input.addEventListener("focus", () => {
+        formGroup.classList.add("has-focus");
+      });
+  
+      input.addEventListener("blur", () => {
+        formGroup.classList.remove("has-focus");
+      });
+    });
+});
+  
+
+window.onload = function() {
+    closeOverlay();
+    /*openOverlay();*/
+}
