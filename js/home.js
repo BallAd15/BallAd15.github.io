df80c7c410596b1e913e31ec2601bf4e52cf1eb7
Aditya Baliga <61866962+BallAd15@users.noreply.github.com>
2023-11-14 16:17:52 +0530

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -3,7 +3,7 @@ function openOverlay() {
   }
   
 function closeOverlay() {
-    document.getElementsByClassName("overlay")[0].style.height = "20%";
+    document.getElementsByClassName("overlay")[0].style.height = "7em";
     document.getElementsByClassName("overlay-about")[0].style.display = "none";
     document.getElementsByClassName("overlay-arrow")[0].style.display = "none";
     document.getElementsByClassName("navbar")[0].style.display = "flex";
@@ -15,13 +15,13 @@ function menubarShow(){
     if ( hiddenDiv.classList.contains('navbar-menu')) {
         hiddenDiv.classList.remove('navbar-menu');
         hiddenDiv.classList.add('visible');
-        document.getElementsByClassName("overlay")[0].style.height = "315px";
+        document.getElementsByClassName("overlay")[0].style.height = "50em";
         document.getElementsByClassName("site-title")[0].style.borderBottom = "1px solid rgb(99, 95, 95)";
         menuIcon.textContent="expand_less"
     } else {
         hiddenDiv.classList.remove('visible');
         hiddenDiv.classList.add('navbar-menu');
-        document.getElementsByClassName("overlay")[0].style.height = "12%";
+        document.getElementsByClassName("overlay")[0].style.height = "7em";
         document.getElementsByClassName("site-title")[0].style.borderBottom = "none";
         menuIcon.textContent="menu"
     }
